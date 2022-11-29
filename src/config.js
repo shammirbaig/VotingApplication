b52d6428b6769507522d507723b359ab26b2921c
shammirbaig <66933858+shammirbaig@users.noreply.github.com>
2022-11-29 12:07:39 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 const config = {
     APP_NAME: 'internal-shammir',
-    API_KEY: '942c8cb8-c72a-4cdc-be4c-1d3ff24fd99e',
-    MONGO_URI:'mongodb+srv://Shammir:Shammir123@cluster0.7091gd0.mongodb.net/?retryWrites=true&w=majority'
+    API_KEY: 'ENTER YOUR API KEY HERE',
+    MONGO_URI:'ENTER YOUR MONGO_URI STRING HERE'
   };
 
   export default config;
